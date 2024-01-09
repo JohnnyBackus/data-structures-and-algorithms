86c90d6cff00883f92e5ba7a9e82c3b7b5a8efed
JohnnyBackus <john.p.backus@gmail.com>
2024-01-09 15:08:02 -0800

    fixed code and completed README

--- python/code_challenges/array_insert_shift/array_insert_shift.py
+++ python/code_challenges/array_insert_shift/array_insert_shift.py
@@ -1,9 +1,17 @@
-test_array_one = [1, 2, 3, 4]
-test_array_two = [1, 2, 3, 4, 5]
+test_list_one = [1, 2, 3, 4]
+test_list_two = [1, 2, 3, 4, 5]
 
-def insert_shift_array(arr, val):
-    first_half = arr[:len(arr)//2]
-    second_half = arr[len(arr)//2:]
+def insert_shift_array(list, val):
+    first_half = []
+    second_half = []
+    for i in range(len(list)):
+        if i <= len(list)//2:
+            first_half.append(list[i])
+        else:
+            second_half.append(list[i])
     first_half.append(val)
-    new_arr = first_half + second_half
-    return new_arr
+    new_list = first_half + second_half
+    return new_list
+
+print(insert_shift_array(test_list_one, 10))
+print(insert_shift_array(test_list_two, 10))
