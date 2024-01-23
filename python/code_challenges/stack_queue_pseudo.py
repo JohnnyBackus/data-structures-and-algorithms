836675cc709301015c5a79987cc579a4a15d0cc3
JohnnyBackus <john.p.backus@gmail.com>
2024-01-23 00:10:20 -0800

    CC11 complete

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,20 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.enqueue_stack = Stack()
+        self.dequeue_stack = Stack()
+
+    def enqueue(self, value):
+        enqueue_stack = self.enqueue_stack
+        dequeue_stack = self.dequeue_stack
+        while dequeue_stack.top is not None:
+            enqueue_stack.push(dequeue_stack.pop())
+        enqueue_stack.push(value)
+
+    def dequeue(self):
+        enqueue_stack = self.enqueue_stack
+        dequeue_stack = self.dequeue_stack
+        while enqueue_stack.top is not None:
+            dequeue_stack.push(enqueue_stack.pop())
+        return dequeue_stack.pop()
